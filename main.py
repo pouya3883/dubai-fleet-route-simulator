6532fa20a7647ae3f01daa6dcba7113d805a640f
Pouya <pouya3883@gmail.com>
2026-06-14 18:08:52 +0400

    feat: add CORS middleware to allow secure frontend dashboard connections

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import sqlite3
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Smart City IoT Ingestion API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 DB_FILE = "smart_city.db"
 
